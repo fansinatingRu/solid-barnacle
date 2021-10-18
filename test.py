5679d8c2bf3b948ceb5e1ee6e02ad8cc387dcb36
fansinatingRu <948908257@qq.com>
2021-10-18 14:00:01 +0800

    Update test.py

--- test.py
+++ test.py
@@ -48,6 +48,7 @@ def fun1(username_text, password_text):
         driver.find_element_by_xpath('// *[ @ id = "load"]').click()
 #         print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + "\t" + str(username_text) + "\n成功登陆")
         print(str(username_text))
+        print('see what happens')
         print(str(password_text))
         time.sleep(3)
         try:
